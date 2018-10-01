3f11fd237887712807dcb3664a01da333bc0874f
BradyCampbell <bradycampbellk12@gmail.com>
2018-10-01 11:11:53 -0600

    Polished the wind effect and got the shake effect attached to the VR camera

---
+++ Acrophobia/Assets/CameraShakeTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class CameraShakeTrigger : MonoBehaviour
+{
+    
+    private void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            CameraShaker.Instance.ShakeOnce(5f, 1f, 60f, 30f);
+            
+        }
+        
+
+        
+    }
+}   
